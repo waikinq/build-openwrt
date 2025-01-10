60b5237e81c67cbe4b7a485b242c05099dc19384
waikinq <waikin@yeah.net>
2025-01-10 23:54:52 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -40,3 +40,4 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
 #
 # ------------------------------- Other ends -------------------------------
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
